57a6b9009741bf59e8892338a9a92ef4ffd2b33a
seoyounglee0105 <os010312@naver.com>
2023-03-16 18:56:58 +0900

    키리스너 추가

--- shopDBGUIProject/src/project/frame/SignUpFrame.java
+++ shopDBGUIProject/src/project/frame/SignUpFrame.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -157,6 +160,16 @@ public class SignUpFrame extends JFrame implements ActionListener, MouseListener
 		idCheckButton.addActionListener(this);
 		phoneCheckButton.addActionListener(this);
 		idTextField.addMouseListener(this);
+		idTextField.addKeyListener(new KeyAdapter() {
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (idHintOff == false) {
+					idTextField.setText("");
+					idHintOff = true;
+				}
+			}
+		});
 		phoneTextField.addMouseListener(this);
 	}
 
